6bef2210d003e12bad74c6b911eccf147707a1ce
Denis Aoki <dnsaoki@gmail.com>
2016-06-22 16:51:20 -0300

    adicionando campo para comentar e remover comentario + testes

--- api/api.py
+++ api/api.py
@@ -1,9 +1,12 @@
 import json
 import requests
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from flask.views import View
+from flask.views import MethodView
 
 app = Flask(__name__)
+
+buffer = {}
 
 class Home(View):
 	def get_news(self, city):
@@ -16,10 +19,26 @@ class Home(View):
 		city = request.args.get('city')
 		news, status = self.get_news(city)
 		if status == 200:
-			return render_template("home.html", news=news), 200
+			return render_template("home.html", news=news, comentarios=buffer.get(city, ''), city=city), 200
 		return render_template("error.html", status=status), status
 
-app.add_url_rule('/news/', view_func=Home.as_view('home'))
+class AddComentario(MethodView):
+	def post(self):
+		city = request.args.get('city')
+		text = request.form['comentario']
+		buffer.setdefault(city, []).append(text)
+		return redirect(url_for('home', city=city))
+
+class RemComentario(MethodView):
+	def post(self):
+		city = request.args.get('city')
+		text = request.args.get('comentario')
+		buffer.get(city, []).remove(text)
+		return redirect(url_for('home', city=city))	
+
+app.add_url_rule('/news/', view_func=Home.as_view('home')) 
+app.add_url_rule('/comentar/', view_func=AddComentario.as_view('comentario'))
+app.add_url_rule('/remover/', view_func=RemComentario.as_view('remove'))
 
 if __name__ == '__main__':
-  app.run(debug=True)
+	app.run(debug=True) 	
--- api/test_api.py
+++ api/test_api.py
@@ -3,11 +3,12 @@ import unittest
 import httpretty
 import json
 
-from flask import Flask
+from flask import Flask, url_for
 
-class ApiTestCase(unittest.TestCase):
+class HomeTestCase(unittest.TestCase):
 	def setUp(self):
 		self.app = api.app.test_client()
+		self.city = 'rj'
 		self.data = [
 			{
 				"subtitulo": "sub1", 
@@ -26,45 +27,71 @@ class ApiTestCase(unittest.TestCase):
 
 	@httpretty.activate
 	def test_get_news_ok(self):
-		httpretty.register_uri(httpretty.GET, "http://c.api.globo.com/news/rj.json", 
+		httpretty.register_uri(httpretty.GET, "http://c.api.globo.com/news/{}.json".format(self.city), 
 			body=json.dumps(self.data),
 			status=200)
 
 		home = api.Home()
-		response, status = home.get_news('rj')
+		response, status = home.get_news(self.city)
 		self.assertEqual(status, 200)
 		self.assertEqual(response, self.data)
 
 	@httpretty.activate
 	def test_get_news_not_found(self):
-		httpretty.register_uri(httpretty.GET, "http://c.api.globo.com/news/rj.json", 
+		httpretty.register_uri(httpretty.GET, "http://c.api.globo.com/news/{}.json".format(self.city), 
 			body='',
 			status=404)
 
 		home = api.Home()
-		response, status = home.get_news('rj')
+		response, status = home.get_news(self.city)
 		self.assertEqual(response, '')
 		self.assertEqual(status, 404)
 
 	@httpretty.activate
 	def test_home_ok(self):
-		
-		httpretty.register_uri(httpretty.GET, "http://c.api.globo.com/news/rj.json", 
+		httpretty.register_uri(httpretty.GET, "http://c.api.globo.com/news/{}.json".format(self.city), 
 			body=json.dumps(self.data),
 			status=200)
-		response = self.app.get("/news/?city=rj")
+		response = self.app.get("/news/?city={}".format(self.city))
 		self.assertEqual(response.status_code, 200)
 		self.assertIn("<title>Flask App</title>", response.data)
 
 	@httpretty.activate
 	def test_home_nok(self):
-		
-		httpretty.register_uri(httpretty.GET, "http://c.api.globo.com/news/rj.json", 
+		httpretty.register_uri(httpretty.GET, "http://c.api.globo.com/news/{}.json".format(self.city), 
 			body='',
 			status=404)
-		response = self.app.get("/news/?city=rj")
+		response = self.app.get("/news/?city={}".format(self.city))
 		self.assertEqual(response.status_code, 404)
 		self.assertIn("<title>Not Found</title>", response.data)
+
+
+class ComentarioTestCase(unittest.TestCase):
+	def setUp(self):
+		self.city = 'rj'
+		self.app = api.app.test_client()
+
+	def tearDown(self):
+		api.buffer = {}
+
+	def test_add_comentario(self):
+		response = self.app.post(
+			"/comentar/?city={}".format(self.city),
+			data={"comentario": "tt"}
+		)
+		self.assertEqual(response.status_code, 302)
+		self.assertEqual(response.location, 'http://localhost/news/?city={}'.format(self.city))
+		self.assertEqual(api.buffer, {"rj": ["tt"]})
+
+	def test_remove_comentario(self):
+		api.buffer[self.city] = ['tt', 'te']
+		response = self.app.post(
+			"/remover/?city={}&comentario={}".format(self.city, "tt")
+		)
+		self.assertEqual(response.status_code, 302)
+		self.assertEqual(response.location, 'http://localhost/news/?city={}'.format(self.city))
+		self.assertEqual(api.buffer, {"rj": ['te']})
+
 
 if __name__ == '__main__':
     unittest.main()
