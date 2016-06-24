43847130f1ecd5d1c2115892c09d541b60f04d70
Denis Aoki <dnsaoki@gmail.com>
2016-06-24 18:12:01 -0300

    format with pep8

--- api/api.py
+++ api/api.py
@@ -8,37 +8,43 @@ app = Flask(__name__)
 
 buffer = {}
 
-class Home(View):
-	def get_news(self, city):
-		req = requests.get('http://c.api.globo.com/news/{}.json'.format(city.lower()))
-		if req.status_code != 200:
-			return '', req.status_code
-		return json.loads(req.text), req.status_code
 
-	def dispatch_request(self):
-		city = request.args.get('city')
-		news, status = self.get_news(city)
-		if status == 200:
-			return render_template("home.html", news=news, comentarios=buffer.get(city, ''), city=city), 200
-		return render_template("error.html", status=status), status
+class Home(View):
+
+    def get_news(self, city):
+        req = requests.get('http://c.api.globo.com/news/{}.json'.format(city.lower()))
+        if req.status_code != 200:
+            return '', req.status_code
+        return json.loads(req.text), req.status_code
+
+    def dispatch_request(self):
+        city = request.args.get('city')
+        news, status = self.get_news(city)
+        if status == 200:
+            return render_template("home.html", news=news, comentarios=buffer.get(city, ''), city=city), 200
+        return render_template("error.html", status=status), status
+
 
 class AddComentario(MethodView):
-	def post(self):
-		city = request.args.get('city')
-		text = request.form['comentario']
-		buffer.setdefault(city, []).append(text)
-		return redirect(url_for('home', city=city))
+
+    def post(self):
+        city = request.args.get('city')
+        text = request.form['comentario']
+        buffer.setdefault(city, []).append(text)
+        return redirect(url_for('home', city=city))
+
 
 class RemComentario(MethodView):
-	def post(self):
-		city = request.args.get('city')
-		text = request.args.get('comentario')
-		buffer.get(city, []).remove(text)
-		return redirect(url_for('home', city=city))	
 
-app.add_url_rule('/news/', view_func=Home.as_view('home')) 
+    def post(self):
+        city = request.args.get('city')
+        text = request.args.get('comentario')
+        buffer.get(city, []).remove(text)
+        return redirect(url_for('home', city=city))
+
+app.add_url_rule('/news/', view_func=Home.as_view('home'))
 app.add_url_rule('/comentar/', view_func=AddComentario.as_view('comentario'))
 app.add_url_rule('/remover/', view_func=RemComentario.as_view('remove'))
 
 if __name__ == '__main__':
-	app.run(debug=True) 	
+    app.run(debug=True)
--- api/test_api.py
+++ api/test_api.py
@@ -3,94 +3,99 @@ import unittest
 import httpretty
 import json
 
-from flask import Flask, url_for
 
 class HomeTestCase(unittest.TestCase):
-	def setUp(self):
-		self.app = api.app.test_client()
-		self.city = 'rj'
-		self.data = [
-			{
-				"subtitulo": "sub1", 
-				"titulo": "tit1", 
-				"url": "url1" 
-			},
-			{
-				"subtitulo": "sub2", 
-				"titulo": "tit2", 
-				"url": "url2"
-			}
-		]
 
-	def tearDown(self):
-		pass
+    def setUp(self):
+        self.app = api.app.test_client()
+        self.city = 'rj'
+        self.data = [
+            {
+                "subtitulo": "sub1",
+                "titulo": "tit1",
+                "url": "url1"
+            },
+            {
+                "subtitulo": "sub2",
+                "titulo": "tit2",
+                "url": "url2"
+            }
+        ]
 
-	@httpretty.activate
-	def test_get_news_ok(self):
-		httpretty.register_uri(httpretty.GET, "http://c.api.globo.com/news/{}.json".format(self.city), 
-			body=json.dumps(self.data),
-			status=200)
+    def tearDown(self):
+        pass
 
-		home = api.Home()
-		response, status = home.get_news(self.city)
-		self.assertEqual(status, 200)
-		self.assertEqual(response, self.data)
+    @httpretty.activate
+    def test_get_news_ok(self):
+        httpretty.register_uri(httpretty.GET,
+                               "http://c.api.globo.com/news/{}.json".format(self.city),
+                               body=json.dumps(self.data),
+                               status=200)
 
-	@httpretty.activate
-	def test_get_news_not_found(self):
-		httpretty.register_uri(httpretty.GET, "http://c.api.globo.com/news/{}.json".format(self.city), 
-			body='',
-			status=404)
+        home = api.Home()
+        response, status = home.get_news(self.city)
+        self.assertEqual(status, 200)
+        self.assertEqual(response, self.data)
 
-		home = api.Home()
-		response, status = home.get_news(self.city)
-		self.assertEqual(response, '')
-		self.assertEqual(status, 404)
+    @httpretty.activate
+    def test_get_news_not_found(self):
+        httpretty.register_uri(httpretty.GET,
+                               "http://c.api.globo.com/news/{}.json".format(self.city),
+                               body='',
+                               status=404)
 
-	@httpretty.activate
-	def test_home_ok(self):
-		httpretty.register_uri(httpretty.GET, "http://c.api.globo.com/news/{}.json".format(self.city), 
-			body=json.dumps(self.data),
-			status=200)
-		response = self.app.get("/news/?city={}".format(self.city))
-		self.assertEqual(response.status_code, 200)
-		self.assertIn("<title>Flask App</title>", response.data)
+        home = api.Home()
+        response, status = home.get_news(self.city)
+        self.assertEqual(response, '')
+        self.assertEqual(status, 404)
 
-	@httpretty.activate
-	def test_home_nok(self):
-		httpretty.register_uri(httpretty.GET, "http://c.api.globo.com/news/{}.json".format(self.city), 
-			body='',
-			status=404)
-		response = self.app.get("/news/?city={}".format(self.city))
-		self.assertEqual(response.status_code, 404)
-		self.assertIn("<title>Not Found</title>", response.data)
+    @httpretty.activate
+    def test_home_ok(self):
+        httpretty.register_uri(httpretty.GET,
+                               "http://c.api.globo.com/news/{}.json".format(self.city),
+                               body=json.dumps(self.data),
+                               status=200)
+        response = self.app.get("/news/?city={}".format(self.city))
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("<title>Flask App</title>", response.data)
+
+    @httpretty.activate
+    def test_home_nok(self):
+        httpretty.register_uri(httpretty.GET,
+                               "http://c.api.globo.com/news/{}.json".format(self.city),
+                               body='',
+                               status=404)
+        response = self.app.get("/news/?city={}".format(self.city))
+        self.assertEqual(response.status_code, 404)
+        self.assertIn("<title>Not Found</title>", response.data)
 
 
 class ComentarioTestCase(unittest.TestCase):
-	def setUp(self):
-		self.city = 'rj'
-		self.app = api.app.test_client()
 
-	def tearDown(self):
-		api.buffer = {}
+    def setUp(self):
+        self.city = 'rj'
+        self.app = api.app.test_client()
 
-	def test_add_comentario(self):
-		response = self.app.post(
-			"/comentar/?city={}".format(self.city),
-			data={"comentario": "tt"}
-		)
-		self.assertEqual(response.status_code, 302)
-		self.assertEqual(response.location, 'http://localhost/news/?city={}'.format(self.city))
-		self.assertEqual(api.buffer, {"rj": ["tt"]})
+    def tearDown(self):
+        api.buffer = {}
 
-	def test_remove_comentario(self):
-		api.buffer[self.city] = ['tt', 'te']
-		response = self.app.post(
-			"/remover/?city={}&comentario={}".format(self.city, "tt")
-		)
-		self.assertEqual(response.status_code, 302)
-		self.assertEqual(response.location, 'http://localhost/news/?city={}'.format(self.city))
-		self.assertEqual(api.buffer, {"rj": ['te']})
+    def test_add_comentario(self):
+        response = self.app.post(
+            "/comentar/?city={}".format(self.city),
+            data={"comentario": "tt"}
+        )
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.location, 'http://localhost/news/?city={}'.format(self.city))
+        self.assertEqual(api.buffer, {"rj": ["tt"]})
+
+    def test_remove_comentario(self):
+        api.buffer[self.city] = ['tt', 'te']
+        response = self.app.post(
+            "/remover/?city={}&comentario={}".format(self.city, "tt")
+        )
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.location, 'http://localhost/news/?city={}'.format(self.city))
+        self.assertEqual(api.buffer, {"rj": ['te']})
 
 
 if __name__ == '__main__':
